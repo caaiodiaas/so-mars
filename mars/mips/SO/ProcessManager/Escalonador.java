e939b25fcd441ea3b1c2ecd5dc6b28333daa1fdb
Caio Dias <89219720+cdblader@users.noreply.github.com>
2022-04-11 21:34:24 -0300

    ajustes tabela de processos

--- mars/mips/SO/ProcessManager/Escalonador.java
+++ mars/mips/SO/ProcessManager/Escalonador.java
@@ -1,11 +1,9 @@
 package mars.mips.SO.ProcessManager;
 
-import java.util.Queue;
-
 public class Escalonador {
 
     public static void escalonar(TabelaDeProcessos table){
-
+            
 
     }
 
--- mars/mips/SO/ProcessManager/TabelaDeProcessos.java
+++ mars/mips/SO/ProcessManager/TabelaDeProcessos.java
@@ -3,8 +3,22 @@ package mars.mips.SO.ProcessManager;
 import java.util.List;
 
 public class TabelaDeProcessos {
+    
     private static List<PCB> prontos;
-    private static List<PCB> execucao;
+    private static PCB execucao;
+
+    public static List<PCB> getProntos() {
+        return prontos;
+    }
+    public static void setProntos(List<PCB> prontos) {
+        TabelaDeProcessos.prontos = prontos;
+    }
+    public static PCB getExecucao() {
+        return execucao;
+    }
+    public static void setExecucao(PCB execucao) {
+        TabelaDeProcessos.execucao = execucao;
+    }
 }
 
 
